6d51f64da4a60a5085ca4e3e925cd192b4f8d8c4
Abdulqudus <abdullateefqudusleeq@gmail.com>
2025-10-24 16:32:36 +0100

    Update route.ts

--- my-app/src/app/api/send/route.ts
+++ my-app/src/app/api/send/route.ts
@@ -10,6 +10,7 @@ export async function POST(req: Request) {
 
     const { data, error } = await resend.emails.send({
       from: 'CleanDoc <onboarding@resend.dev>',
+to: email,
       subject: 'Welcome to CleanDoc 🧼',
       react: EmailTemplate({ firstName: name }),
     })
